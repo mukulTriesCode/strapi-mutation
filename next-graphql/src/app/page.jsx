c05854d74777bfcdbf6147306afc537997913f6f
Mukul Sharma <mukulsharma262002@gmail.com>
2024-10-22 12:51:15 +0530

    Added isChanged state and passed props

--- next-graphql/src/app/page.jsx
+++ next-graphql/src/app/page.jsx
@@ -6,13 +6,15 @@ import { ApolloProvider } from "@apollo/client";
 import CommentInput from "@/components/CommentInput";
 
 export default function Home() {
+  const [isChanged, setIsChanged] = useState(false);
+
   return (
     <ApolloProvider client={graphqlClient}>
       <div className="h-screen text-center grid place-items-center">
         <div className="container">
           <h1 className="text-3xl mb-8">Genres</h1>
-          <CommentInput />
-          <Demo />
+          <CommentInput isChanged={isChanged} setIsChanged={setIsChanged} />
+          <Demo isChanged={isChanged} />
         </div>
       </div>
     </ApolloProvider>
